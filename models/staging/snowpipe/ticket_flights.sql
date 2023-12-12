681a320cd2c68723239fda0b4f0e9fe43da1093f
Levy Shemtov <leviizu@gmail.com>
2023-12-12 06:43:37 +0000

    build staging models

---
+++ models/staging/snowpipe/ticket_flights.sql
@@ -0,0 +1,22 @@
+with 
+ticket_flights as (
+
+    select *
+    from {{ source('air_travel', 'ticket_flights') }}
+
+)
+
+,ticket_flights_renamed as (
+
+    select 
+       ticket_no   
+      ,flight_id         
+      ,fare_conditions     
+      ,amount
+    from 
+        ticket_flights
+ 
+)
+
+select * 
+from ticket_flights_renamed
